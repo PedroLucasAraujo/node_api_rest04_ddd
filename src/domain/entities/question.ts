b6bbe90fb7966c37319815db78c3ada7c68c3102
PedroLucasAraujo <pedro.lucas@almahcomunicacao.com.br>
2025-03-10 19:14:45 -0300

    feat: mapping properties

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,10 +1,14 @@
-import type { Slug } from "./value-objects/slug";
+import { Slug } from "./value-objects/slug";
 import { Entity } from "../../core/entities/entity";
+import type { UniqueEntityId } from "../../core/entities/unique-entity-id";
 
 interface QuestionProps {
+  authorId: UniqueEntityId;
+  bestAnswerId?: UniqueEntityId;
   title: string;
   content: string;
   slug: Slug;
-  authorId: string;
+  createdAt: Date;
+  updatedAt?: Date;
 }
 export class Question extends Entity<QuestionProps> {}
