cc71bfba91905c1477dd5b50bc02d6bd6adb8567
PedroLucasAraujo <pedro.lucas@almahcomunicacao.com.br>
2025-03-20 17:29:28 -0300

    feat: create fetch answers and question comments use case and unit tests

--- test/repositories/in-memory-answer-comments-repository.ts
+++ test/repositories/in-memory-answer-comments-repository.ts
@@ -1,3 +1,4 @@
+import { PaginationParams } from "@/core/repositories/pagination-params";
 import { AnswerCommentsRepository } from "@/domain/forum/application/repositories/answer-comments-repository";
 import { AnswerComment } from "@/domain/forum/enterprise/entities/answer-comment";
 
@@ -5,10 +6,6 @@ export class InMemoryAnswerCommentsRepository
   implements AnswerCommentsRepository
 {
   public items: AnswerComment[] = [];
-
-  async create(answerComment: AnswerComment) {
-    this.items.push(answerComment);
-  }
 
   async findById(id: string) {
     const answerComment = this.items.find((item) => item.id.toString() === id);
@@ -18,6 +15,18 @@ export class InMemoryAnswerCommentsRepository
     }
 
     return answerComment;
+  }
+
+  async findManyByAnswerId(answerId: string, { page }: PaginationParams) {
+    const answerComments = this.items
+      .filter((item) => item.answerId.toString() === answerId)
+      .slice((page - 1) * 20, page * 20);
+
+    return answerComments;
+  }
+
+  async create(answerComment: AnswerComment) {
+    this.items.push(answerComment);
   }
 
   async delete(answerComment: AnswerComment) {
