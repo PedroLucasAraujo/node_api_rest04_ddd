8ab99fb4f6a27048041cc5bcf0430b7fdb827d0d
PedroLucasAraujo <pedro.lucas@almahcomunicacao.com.br>
2025-03-20 14:01:40 -0300

    feat: add get question by slug use case and unit test

--- src/domain/forum/application/use-cases/create-question.ts
+++ src/domain/forum/application/use-cases/create-question.ts
@@ -1,6 +1,6 @@
 import { UniqueEntityId } from "@/core/entities/unique-entity-id";
 import { Question } from "../../enterprise/entities/question";
-import { QuestionsRepository } from "../repositories/questions-srepository";
+import { QuestionsRepository } from "../repositories/questions-repository";
 
 interface CreateQuestionUseCaseRequest {
   authorId: string;
--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -1,9 +1,18 @@
-import type { QuestionsRepository } from "@/domain/forum/application/repositories/questions-srepository";
+import type { QuestionsRepository } from "@/domain/forum/application/repositories/questions-repository";
 import type { Question } from "@/domain/forum/enterprise/entities/question";
 
 export class InMemoryQuestionsRepository implements QuestionsRepository {
   public items: Question[] = [];
 
+  async findBySlug(slug: string): Promise<Question | null> {
+    const question = this.items.find((item) => item.slug.value === slug);
+
+    if (!question) {
+      return null;
+    }
+
+    return question;
+  }
   async create(question: Question) {
     this.items.push(question);
   }
