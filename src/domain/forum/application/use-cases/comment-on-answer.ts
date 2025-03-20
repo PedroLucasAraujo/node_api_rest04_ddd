236b206a8bcc5c2d475623c4b86e57ebd9360877
PedroLucasAraujo <pedro.lucas@almahcomunicacao.com.br>
2025-03-20 16:58:56 -0300

    feat: create comment on answers and question use case and unit tests

---
+++ src/domain/forum/application/use-cases/comment-on-answer.ts
@@ -0,0 +1,44 @@
+import { AnswersRepository } from "../repositories/answers-repository";
+import { AnswerComment } from "../../enterprise/entities/answer-comment";
+import type { AnswerCommentsRepository } from "../repositories/answer-comments-repository";
+import { UniqueEntityId } from "@/core/entities/unique-entity-id";
+
+interface CommentOnAnswerUseCaseRequest {
+  authorId: string;
+  answerId: string;
+  content: string;
+}
+
+interface CommentOnAnswerUseCaseResponse {
+  answerComment: AnswerComment;
+}
+
+export class CommentOnAnswerUseCase {
+  constructor(
+    private answersRepository: AnswersRepository,
+    private answersCommentsRepository: AnswerCommentsRepository
+  ) {}
+  async execute({
+    authorId,
+    content,
+    answerId,
+  }: CommentOnAnswerUseCaseRequest): Promise<CommentOnAnswerUseCaseResponse> {
+    const answer = await this.answersRepository.findById(answerId);
+
+    if (!answer) {
+      throw new Error("Answer not found.");
+    }
+
+    const answerComment = AnswerComment.create({
+      authorId: new UniqueEntityId(authorId),
+      answerId: new UniqueEntityId(answerId),
+      content,
+    });
+
+    await this.answersCommentsRepository.create(answerComment);
+
+    return {
+      answerComment,
+    };
+  }
+}
---
+++ src/domain/forum/application/use-cases/comment-on-question.ts
@@ -0,0 +1,44 @@
+import { QuestionsRepository } from "../repositories/questions-repository";
+import { QuestionComment } from "../../enterprise/entities/question-comment";
+import type { QuestionCommentsRepository } from "../repositories/question-comments-repository";
+import { UniqueEntityId } from "@/core/entities/unique-entity-id";
+
+interface CommentOnQuestionUseCaseRequest {
+  authorId: string;
+  questionId: string;
+  content: string;
+}
+
+interface CommentOnQuestionUseCaseResponse {
+  questionComment: QuestionComment;
+}
+
+export class CommentOnQuestionUseCase {
+  constructor(
+    private questionsRepository: QuestionsRepository,
+    private questionsCommentsRepository: QuestionCommentsRepository
+  ) {}
+  async execute({
+    authorId,
+    content,
+    questionId,
+  }: CommentOnQuestionUseCaseRequest): Promise<CommentOnQuestionUseCaseResponse> {
+    const question = await this.questionsRepository.findById(questionId);
+
+    if (!question) {
+      throw new Error("Question not found.");
+    }
+
+    const questionComment = QuestionComment.create({
+      authorId: new UniqueEntityId(authorId),
+      questionId: new UniqueEntityId(questionId),
+      content,
+    });
+
+    await this.questionsCommentsRepository.create(questionComment);
+
+    return {
+      questionComment,
+    };
+  }
+}
