8fb331a5de74727bcf075df1339597126ded87e5
PedroLucasAraujo <pedro.lucas@almahcomunicacao.com.br>
2025-03-20 15:40:29 -0300

    feat: create fetch recents questions with pagination use case and unit test

--- src/domain/forum/application/repositories/questions-repository.ts
+++ src/domain/forum/application/repositories/questions-repository.ts
@@ -1,8 +1,10 @@
+import type { PaginationParams } from "@/core/repositories/pagination-params";
 import { Question } from "../../enterprise/entities/question";
 
 export interface QuestionsRepository {
   create(question: Question): Promise<void>;
   findById(id: string): Promise<Question | null>;
+  findManyRecent(params: PaginationParams): Promise<Question[]>;
   findBySlug(slug: string): Promise<Question | null>;
   save(question: Question): Promise<void>;
   delete(question: Question): Promise<void>;
