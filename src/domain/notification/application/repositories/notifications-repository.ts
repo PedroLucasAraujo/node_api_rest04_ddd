8bc94b0e5547487e9dff289fc08bbcbc84510ee3
PedroLucasAraujo <pedro.lucas@almahcomunicacao.com.br>
2025-04-04 13:46:47 -0300

    feat: create read notificatio use case and test

--- src/domain/notification/application/repositories/notifications-repository.ts
+++ src/domain/notification/application/repositories/notifications-repository.ts
@@ -3,5 +3,5 @@ import { Notification } from "../../enterprise/entities/notification";
 export interface NotificationsRepository {
   create(notification: Notification): Promise<void>;
   findById(id: string): Promise<Notification | null>;
-  create(notification: Notification): Promise<void>;
+  save(notification: Notification): Promise<void>;
 }
